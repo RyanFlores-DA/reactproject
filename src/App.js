ac080647f7eb55f7f08253535190595234d57af4
RyanFlores-DA <ryan.designe.ux@outlook.com>
2021-11-12 20:58:31 -0300

    lista atualizada

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import api from './services/api';
 
 function App() {
+    //iniciar um array vazio pra guardar a lista de tasks
+    const[tasks, setTasks] = useState([])
+
     useEffect(() => {
         api.get('posts').then(response => {
-            console.log(response.data)
+            setTasks(response.data)
         })
     }, [])
     return ( 
-        <div className = "App" >
-        <header className = "App-header" >
-        <img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <p>
-         Edit < code > src / App.js </code> and save to reload. </p>
-        <a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React 
-        </a> 
-        </header > 
+        <div className="App">
+            <header className="App-header">
+            <h1>Todo List - React</h1>
+            </header>
+            <main>
+                <ul>
+                    {
+                    tasks.map(tasks =>{
+                        return ( 
+                            <li>
+                                <h5>{tasks.title}</h5>
+                                <input type="checkbox" checked={tasks.completed} />
+                            </li>
+                            )
+                    })
+                    }
+                    
+                </ul>
+            </main>
         </div>
     );
 }
