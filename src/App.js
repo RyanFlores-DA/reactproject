8ccb4c31d97d37e4858d59c7a245a5ae43f241da
RyanFlores-DA <ryan.designe.ux@outlook.com>
2021-11-19 20:58:59 -0300

    pastas adicionadas e componentes criados

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import './App.css';
-import { useEffect, useState } from 'react'
-import api from './services/api';
+import Header from './components/Header'
+import Main from './components/taskList'
+import Footer from './components/Footer';
+
 
 function App() {
-    //iniciar um array vazio pra guardar a lista de tasks
-    const[tasks, setTasks] = useState([])
 
-    useEffect(() => {
-        api.get('posts').then(response => {
-            setTasks(response.data)
-        })
-    }, [])
+    const user = {
+        id: 1,
+        name: 'Ryan Flores'
+    }
     return ( 
         <div className="App">
-            <header className="App-header">
-            <h1>Todo List - React</h1>
-            </header>
-            <main>
-                <ul>
-                    {
-                    tasks.map(tasks =>{
-                        return ( 
-                            <li>
-                                <h5>{tasks.title}</h5>
-                                <input type="checkbox" checked={tasks.completed} />
-                            </li>
-                            )
-                    })
-                    }
-                    
-                </ul>
-            </main>
+            <Header listTitle={'Estudos'} user={user}/>
+            <Main/>
+           <Footer/>
         </div>
     );
 }
